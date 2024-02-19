e01e512b29c2dbcf96bfeda67a3fb1bb3655e807
TiffanyASU <khubbar4@asu.edu>
2024-02-18 20:29:58 -0700

    Lots of changes!
    Removed unused files. New feedback form, new account form, existing feedback list, etc.

--- FMS.js
+++ FMS.js
@@ -1,13 +1,27 @@
 function main() {
     accountList = [];
-    console.log("Page Loaded, Ready.");
+    // console.log("Page Loaded, Ready.");
     checkLoginStatus();
-    console.log("Checked login status!")
+    // console.log("Checked login status!")
     loadAccounts();
+
 
     if (window.location.href.includes("manageAccounts.html")) {
         console.log("Current Page: Manage Accounts");
-        manageAccountsPage();
+        // manageAccountsPage();
+    }
+    else if (window.location.href.includes("newAccount.html")) {
+        console.log("Current Page: New Account");
+    }
+    else if (window.location.href.includes("feedback.html")) {
+        console.log("Current Page: Feedback");
+        loadFeedback();
+        createFeedbackList();
+    }
+    else if (window.location.href.includes("newFeedback.html")) {
+        console.log("Current Page: Enter New Feedback");
+        loadFeedback();
+        newFeedbackInfo();
     }
     else {
         console.log("Current Page: The Void. How are you seeing this text?");
@@ -19,39 +33,32 @@ function loadAccounts() {
     if (localStorage.getItem('users') == null) {
         var newAccount = {};
 
-        // Adding some default accounts for now.  Will translate tino DB later.
         newAccount = { firstName: "Tiffany", lastName: "Hubbard", level: "employee", username: "tHubbard", password: "123abc"};
         accountList.push(newAccount);
-        console.log("Employee Tiffany Added.");
         newAccount = { firstName: "Bryan", lastName: "Thomas", level: "manager", username: "bThomas", password: "abcdef" };
         accountList.push(newAccount);
-        console.log("Manager Bryan Added.");
         newAccount = { firstName: "Frida", lastName: "Ayala Bojorquez", level: "employee", username: "fBojorquez", password: "123456" };
         accountList.push(newAccount);
-        console.log("Employee Frida Added.");
         newAccount = { firstName: "Haseen", lastName: "Aman", level: "employee", username: "hAman", password: "acb123" };
         accountList.push(newAccount);
-        console.log("Employee Haseen Added.");
         newAccount = { firstName: "Keelia", lastName: "Kwan", level: "manager", username: "kKwan", password: "a1c2b3" };
         accountList.push(newAccount);
-        console.log("Manager Keelia Added.");
-        newAccount = { firstName: "Test", lastName: "Employee", level: "employee", username: "employee", password: "employee123" };
+        newAccount = { firstName: "TestEmployee", lastName: "Employee", level: "employee", username: "employee", password: "employee123" };
         accountList.push(newAccount);
-        console.log("Test Employee Added.");
-        newAccount = { firstName: "Test", lastName: "Manager", level: "manager", username: "manager", password: "manager123" };
+        newAccount = { firstName: "TestManager", lastName: "Manager", level: "manager", username: "manager", password: "manager123" };
         accountList.push(newAccount);
-        console.log("Test Manager Added.");
-        newAccount = { firstName: "Test", lastName: "Admin", level: "admin", username: "admin", password: "admin123" };
+        newAccount = { firstName: "TestAdmin", lastName: "Admin", level: "admin", username: "admin", password: "admin123" };
         accountList.push(newAccount);
-        console.log("Test Admin Added.");
-        console.log(accountList);
         console.log("Created default users.");
+        console.log("Account List:");
+        console.log(accountList);
         localStorage.setItem("users", JSON.stringify(accountList));
     }
     else {
         accountList = JSON.parse(localStorage.getItem("users"));
-        console.log(accountList);
         console.log("Loaded Accounts List from Local Storage");
+        console.log("Account List:");
+        console.log(accountList);
     }
 }
 
@@ -69,7 +76,7 @@ function manageAccountsLink() {
 
 function enterFeedbackLink() {
     if (userLevel === "manager") { 
-        document.location.href = "enterFeedback.html";
+        document.location.href = "feedback.html";
     }
     else if (userLevel === "employee" || userLevel === "admin") {
         document.location.href = "noPermissions.html";
@@ -98,9 +105,7 @@ function logoutLink() {
 function login() {
     console.log(accountList);
     let enteredUsername = document.getElementById('username').value;
-    // console.log("Entered Username is " + enteredUsername);
     let enteredPassword = document.getElementById('password').value;
-    // console.log("Entered Password is " + enteredPassword);
 
     let accountFound = accountList.find(account => account.username === enteredUsername && account.password === enteredPassword);
 
@@ -131,73 +136,135 @@ function checkLoginStatus() {
     console.log("Current User from Session Storage: " + currentAccount + " " + userLevel + " " + loggedIn);
 }
 
+function newAccountPage() {
+    document.location.href = "newAccount.html";
+}
 
+function createNewAccount() {
+    var newAccount = {};
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let accountType = document.getElementById('accountTypes').value;
 
+    newAccount = { firstName: firstName, lastName: lastName, level: accountType, username: username, password: password };
+    accountList.push(newAccount);
+    localStorage.setItem("users", JSON.stringify(accountList));
+}
 
-function manageAccountsPage() {
-    /*
-    accountCount = accountList.length
-    console.log(accountCount);
-    itemCount = 0;
+function newFeedbackPage() {
+    document.location.href = "newFeedback.html";
+}
 
-    tempAccountList = JSON.stringify(accountList)
-    console.log(tempAccountList)
+function loadFeedback() {
+    feedbackList = [];
 
-    var accountsTable = document.getElementById("accountsTable");
+    if (localStorage.getItem('feedbacks') == null) {
+        var feedback = { entryManager: "", employee: "", date: "", department: "", feedback: ""};
 
+        feedback = { entryManager: "Keelia", employee: "Tiffany", date: "2024.01.01", department: "HR", feedback: "There needs to be more office parties."};
+        feedbackList.push(feedback);
+        feedback = { entryManager: "Keelia", employee: "Tiffany", date: "2024.01.08", department: "Accounting", feedback: "Employees should have more freedom with business expenses."};
+        feedbackList.push(feedback);
+        feedback = { entryManager: "Keelia", employee: "Tiffany", date: "2023.12.12", department: "Sales", feedback: "The benchmark sales figures are unrealistic."};
+        feedbackList.push(feedback);
+        feedback = { entryManager: "Keelia", employee: "Frida", date: "2024.02.05", department: "Sales", feedback: "Need more Customer Support."};
+        feedbackList.push(feedback);
+        feedback = { entryManager: "Keelia", employee: "Frida", date: "2024.01.12", department: "Sales", feedback: "Sales incentives need to be increased."};
+        feedbackList.push(feedback);
+        feedback = { entryManager: "Bryan", employee: "Haseen", date: "2023.12.15", department: "HR", feedback: "Our manager always comes into the office late, but write us up if we do."};
+        feedbackList.push(feedback);
+        feedback = { entryManager: "Bryan", employee: "Haseen", date: "2024.01.06", department: "HR", feedback: "Actual coffee instead of instant coffee is needed."};
+        feedbackList.push(feedback);
+        feedback = { entryManager: "Bryan", employee: "Haseen", date: "2024.02.13", department: "Development", feedback: "We need more time off after weeks of grinding."};
+        feedbackList.push(feedback);
+        feedback = { entryManager: "Bryan", employee: "Haseen", date: "2024.01.20", department: "Accounting", feedback: "Larger pay raises are need to combat market."};
+        feedbackList.push(feedback);
 
-    tempAccountList.forEach(addTableRow);
-    function addTableRow() {
-        for (const [key, value] of Object.entries(tempAccountList)) {
-            console.log(key, value);
-            var row = accountsTable.insertRow(-1);
-            var usernameCell = row.insertCell(0);
-            usernameCell.innerHTML = value;
-            //var firstNameCell
-            //var lastNameCell
-            //var accountTypeCell
-            listIndex += 1;
+        console.log("Created default feedback.");
+        console.log("Feedback List:");
+        console.log(feedbackList);
+        localStorage.setItem("feedbacks", JSON.stringify(feedbackList));;
+    }
+    else {
+        feedbackList = JSON.parse(localStorage.getItem("feedbacks"));
+        console.log("Loaded existing feedback from Local Storage");
+        console.log("Feedback List:");
+        console.log(feedbackList);
+    }
+}
+
+function createFeedbackList() {
+    var feedbackCount = feedbackList.length
+    console.log("Number of feedback: " + feedbackCount);
+    currentIndex = 0;
+
+    var sortedFeedback = feedbackList.toSorted( compare );
+
+    function compare( a, b ) {
+        if ( a.date < b.date ){
+          return 1;
         }
-    }
+        if ( a.date > b.date ){
+          return -1;
+        }
+        return 0;
+      }
 
-    for (const [key, value] of Object.entries(accountList)) {
-        console.log(key, value);
-        var row = accountsTable.insertRow(-1);
-        var usernameCell = row.insertCell(0);
-        usernameCell.innerHTML = value;
-        //var firstNameCell
-        //var lastNameCell
-        //var accountTypeCell
+    while (currentIndex < feedbackCount) {
+        let row = feedbackTable.insertRow(-1);
+        let dateCell = row.insertCell(0);
+        dateCell.innerHTML = sortedFeedback[currentIndex].date;
+        let employeeCell = row.insertCell(1);
+        employeeCell.innerHTML = sortedFeedback[currentIndex].employee;
+        let managerCell = row.insertCell(2);
+        managerCell.innerHTML = sortedFeedback[currentIndex].entryManager;
+        let departmentCell = row.insertCell(3);
+        departmentCell.innerHTML = sortedFeedback[currentIndex].department;
+        let feedbackCell = row.insertCell(4);
+        feedbackCell.innerHTML = sortedFeedback[currentIndex].feedback;
+        currentIndex++;
     }
-
-   // This is on the right track!
-    do {
-        console.log(tempAccountList[itemCount]);
-        itemCount++;
-    } while (itemCount < accountCount);
-    */
 }
 
+function newFeedbackInfo() {
+    const todaysDate = new Date();
+    let year = todaysDate.getFullYear();
+    let month = String(todaysDate.getMonth() + 1).padStart(2,"0");
+    let day = String(todaysDate.getDate()).padStart(2,"0");
+    let formattedDate = year + "." + month + "." + day;
+    currentDate.innerHTML = formattedDate;
 
+    let storedUser = JSON.parse(sessionStorage.getItem("storedUser"));
+    manager.innerHTML = storedUser.currentAccount;
 
-
-
-
-
-
-
-
-function newAccount() {
-    // This will be plugged into the UI for new account
-    let firstName = "";
-    let lastName = "";
-    let manager = false;
-    let password = "";
-    let username = "";
-
-    // Add code here to create new account
+    var numberOfAccounts = accountList.length;
+    var currentIndex = 0;
+    var selectedMenu = document.getElementById("employee");
+    while ( currentIndex < numberOfAccounts ) {
+        if (accountList[currentIndex].level == "employee") {
+            var option = document.createElement('option');
+            option.text = option.value = accountList[currentIndex].firstName;
+            selectedMenu.add(option, -1)
+        }
+        else {
+        }
+        currentIndex++;
+    }
 }
 
+function createNewFeedback() {
+    var newFeedback = {};
+    let date = document.getElementById('currentDate').textContent;
+    let manager = document.getElementById('manager').textContent;
+    let employee = document.getElementById('employee').value;
+    let department = document.getElementById('department').value;
+    let feedback = document.getElementById('feedbackArea').value;
 
+    newFeedback = { entryManager: manager, employee: employee, date: date, department: department, feedback: feedback};
+    feedbackList.push(newFeedback);
+    localStorage.setItem("feedbacks", JSON.stringify(feedbackList));
+}
 
 $(document).ready(main);
